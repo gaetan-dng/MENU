6ae82786215df1a619867e58f8e2c11d30add7e2
gaetan-dng <70692819+gaetan-dng@users.noreply.github.com>
2020-12-17 11:52:14 +0100

    1 sur 3

---
+++ menu.php
@@ -0,0 +1,82 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="page_menu.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet"> 
+        <title>MENU</title>
+    </head>
+    <body>       
+        <div class="navbar">
+            <nav>
+                <ul>
+                    <li><a href="#">HTML</a>
+                        <ul>
+                            <li><a href="EXO/exo1.html">exo 1</a></li>
+                            <li><a href="EXO/exo2.html">exo 2</a></li>
+                            <li><a href="EXO/exo3.html">exo 3</a></li>
+                            <li><a href="EXO/exo4.html">exo 4</a></li>
+                            <li><a href="EXO/exo6.html">exo 6</a></li>
+                            <li><a href="EXO/Pages_FINAL/Page_accueilF.html">exo final</a></li>
+                        </ul>
+                    </li>
+                    
+                    <li><a href="#">CSS</a>
+                        <ul>
+                            <li><a href="EXO/exo1CSS.html">exo 1</a></li>
+                            <li><a href="EXO/exo2CSS.html">exo 2</a></li>
+                            <li><a href="EXO/exo3CSS.html">exo 3</a></li>
+                            <li><a href="EXO/exo4CSS.html">exo 4</a></li>
+                            <li><a href="EXO/Pages_FINAL/Page_accueilF.html">exo final</a></li>
+                        </ul>
+                    </li>
+                    
+                    <li><a href="#">PHP</a>
+                        <ul>
+                            <li><a href="EXO/BDD_TD1_Exo1.php">Partie 1 exo 1</a></li>
+                            <li><a href="EXO/BDD_TD1_Exo2.php">Partie 1 exo 2</a></li>
+                            <li><a href="EXO/BDD_TD1_Exo3.php">Partie 1 exo 3</a></li>
+                            <li><a href="EXO/BDD_TD1_Exo4.php">Partie 1 exo 4</a></li>
+                            <li><a href="EXO/BDD_TD1_Exo5.php">Partie 1 exo 5</a></li>
+                            <li><a href="EXO/BDD_TD1_Exo6.php">Partie 1 exo 6</a></li>
+                            <li><a href="EXO/BDD_TD1_Exo7.php">Partie 1 exo 7</a></li>
+                            <li><a href="EXO/BDD_TD2_Exo1.php">Partie 1 exo 1</a></li>
+                            <li><a href="EXO/BDD_TD2_Exo2.php">Partie 2 exo 2</a></li>
+                            <li><a href="EXO/BDD_TD2_Exo3.php">Partie 2 exo 3</a></li>
+                            <li><a href="EXO/fonction.php">fonction</a></li>
+                        </ul>
+                    </li>
+                    
+                    <li><a href="#">BDD</a>
+                        <ul>
+                            <li><a href="EXO/EXO1.php">exo 1</a></li>
+                            <li><a href="EXO/EXO2.php">exo 2</a></li>
+                            <li><a href="EXO/EXO3.php">exo 3</a></li>
+                            <li><a href="EXO/EXO4.php">exo 4</a></li>
+                            <li><a href="EXO/EXO5.php">exo 5</a></li>
+                            <li><a href="EXO/EXO6.php">exo 6</a></li>
+                            <li><a href="EXO/EXO7.php">exo 7</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a href="#">SQL</a>
+                        <ul>
+                            <li><a href="EXO/SQL_TD3_Exo2.php">TD 3 exo 2</a></li>
+                            <li><a href="EXO/SQL_TD4_Exo1.php">TD 4 exo 1</a></li>
+                            <li><a href="EXO/SQL_TD4_Exo2.php">TD 4 exo 2</a></li>
+                            <li><a href="EXO/SQL_TD4_Exo3.php">TD 4 exo 3</a></li>
+                            <li><a href="EXO/fonction_SQL.php">fonction</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="fin"><a href="menu.php">MENU</a></li>
+                </ul>
+            </nav>
+        </div>
+    </body>
+</html>
